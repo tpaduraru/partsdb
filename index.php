3369b33ae7bf6761f4065af73fb4cf9fe15d1381
Timotei Paduraru <timopaduraru@gmail.com>
2018-12-29 18:24:14 -0800

    now prints the table nicely in html

--- index.php
+++ index.php
@@ -1,9 +1,37 @@
-<?php
-	$db = new SQLite3('parts.db');
-	$sql = 'select * from NorthHighlands;';
-	$result = $db->query($sql);
-	while ($row=$result->fetchArray())
-	{
-		var_dump($row);
-	}
-?>
+<html>
+	<head>
+		<title>Tekniton North Highlands Stock</title>
+	</head>
+	<body>
+		<?php
+			$db = new SQLite3('parts.db');
+			$sql = 'SELECT number, quantity, description FROM NorthHighlands;';
+			$result = $db->query($sql);
+		?>
+		<table>
+			<tr>
+				<th>Part Number</th>
+				<th>Quantity</th>
+				<th>Description</th>
+			</tr>
+			<?php
+				while ($row=$result->fetchArray())
+				{
+					echo "<tr>";
+					echo "<td>";
+					echo $row[number];
+					echo "</td>";
+					echo "<td>";
+					echo $row[quantity];
+					echo "</td>";
+					echo "<td>";
+					echo $row[description];
+					echo "</td>";
+					echo "</tr>";
+				}
+			?>
+		</table>
+	</body>
+</html>
+
+
